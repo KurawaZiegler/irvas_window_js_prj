0188e4feb0957078f7ac5511292aef7eb1af60ba
Alina <example@gmail.com>
2026-05-18 20:01:58 +0300

    init new js project for interactive on page

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let path = require('path');
+
+module.exports = {
+    mode: 'development',
+    // entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        // path: path.resolve(__dirname, 'dist/js')
+    },
+    watch: true,
+
+    devtool: "source-map",
+
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env', {
+                                debug: true,
+                                corejs: 3,
+                                useBuiltIns: 'usage'
+                            }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
